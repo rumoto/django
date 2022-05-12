fa52e06efbb795a97503918c1e4e1749c99229eb
Sergey Remnev <serg_yar@inbox.ru>
2022-05-12 16:43:58 +0300

    models added at models.py

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,13 @@
 from django.db import models
-
+#d;name;image;price;release_date;lte_exists;slug
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
-    pass
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    image = models.CharField(max_length=200)
+    price = models.FloatField(max_length=10)
+    release_date = models.CharField(max_length=20)
+    lte_exists = models.BooleanField()
+    slug = models.CharField(max_length=50)
+
